a28dea626f5acf9c68d070a741f599814a28056e
Tommaso Panozzo <t.panozzo@miriade.it>
2021-04-09 18:22:53 +0200

    feat: add divisors endpoint in go

--- ec2/go/main.go
+++ ec2/go/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/go/greetings", func(c *gin.Context) {
+	r.GET("/greetings", func(c *gin.Context) {
 		name := c.DefaultQuery("name", "world")
 		c.JSON(200, gin.H{
 			"message": fmt.Sprintf("Hello, %s!", name),
 		})
 	})
-  r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.GET("/numbers/:n/divisors", func(c *gin.Context) {
+		nstr := c.Param("n")
+		n, _ := strconv.Atoi(nstr)
+
+		var aux []int
+
+		for i := 1; i <= n; i++ {
+			if n%i == 0 {
+				aux = append(aux, i)
+			}
+		}
+
+		c.JSON(200, aux)
+	})
+	r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
